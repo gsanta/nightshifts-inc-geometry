68163a4b2e72e9c5542f7da0e2c7d7cb572b985b
gsanta <gsanta@chemaxon.com>
2019-06-25 21:58:32 +0200

    isDistanceSmallerThanOneUnit made static.

--- src/utils/MeasurementUtils.ts
+++ src/utils/MeasurementUtils.ts
@@ -2,7 +2,7 @@ import { Point } from "../shapes/Point";
 
 export class MeasurementUtils {
 
-    public isDistanceSmallerThanOneUnit(point1: Point, point2: Point) {
+    public static isDistanceSmallerThanOneUnit(point1: Point, point2: Point) {
         return point1.distanceTo(point2) <= 1;
     }
 }
