5297ef4916ca9b6f22d0ececa7873f458cb81659
gsanta <gsanta@chemaxon.com>
2019-06-28 19:20:58 +0200

    isDistanceSmallerThan made configurable.

--- src/utils/MeasurementUtils.ts
+++ src/utils/MeasurementUtils.ts
@@ -1,8 +1,8 @@
 import { Point } from "../shapes/Point";
 
 export class MeasurementUtils {
-
-    public static isDistanceSmallerThanOneUnit(point1: Point, point2: Point) {
-        return point1.distanceTo(point2) <= 1;
+    // TODO: rename to smth like "closeTo" so that the exact distance is not mentioned in the name
+    public static isDistanceSmallerThan(point1: Point, point2: Point, unit = 0.5) {
+        return point1.distanceTo(point2) <= unit;
     }
 }
